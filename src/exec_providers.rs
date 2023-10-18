f50922566e2cf451a82e559c8bb5d65c527c3032
uthree <uthree271828@gmail.com>
2023-10-18 23:47:14 +0900

    tmp

--- src/exec_providers.rs
+++ src/exec_providers.rs
@@ -1,25 +1,11 @@
-use ort::execution_providers::{CUDAExecutionProviderOptions, ExecutionProvider};
+use ort::execution_providers::ExecutionProvider;
 
 pub fn available_providers() -> Vec<ExecutionProvider> {
     let mut output = Vec::new();
     output.push(ExecutionProvider::CPU(Default::default()));
-    let mut id: u32 = 0;
-    loop {
-        let mut opt: CUDAExecutionProviderOptions = Default::default();
-        opt.device_id = id;
-        let mut prov = ExecutionProvider::CUDA(opt);
-        match prov {
-            ExecutionProvider::CUDA(ref mut cuda_opt) => {
-                cuda_opt.device_id = id;
-            }
-            _ => unreachable!(),
-        }
-        if prov.is_available() {
-            output.push(prov);
-        } else {
-            break;
-        }
-        id += 1;
+    let cuda_provider = ExecutionProvider::CUDA(Default::default());
+    if cuda_provider.is_available() {
+        output.push(cuda_provider);
     }
     output
 }
--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,17 @@ use ort::ExecutionProvider;
 use std::path::Path;
 
 fn main() {
-    /*
     let provider = ExecutionProvider::CUDA(Default::default());
-    let mut model = VoiceConvertor::load(&Path::new("../zvc-dev/onnx/"), provider).unwrap();
+    println!("Loading model...");
+    let mut model = VoiceConvertor::load(&Path::new("./onnx/"), provider).unwrap();
+    println!("Loading wave file");
     let (header, wf) = wave_file::load(Path::new("test.wav"));
+    println!("Converting...");
     let wf = resample(wf, header.sampling_rate, 16000);
     let wf = model.convert(wf, 0.0, 0.1).unwrap();
     let wf = resample(wf, 16000, header.sampling_rate);
+    println!("Saving...");
     wave_file::save(header, wf, Path::new("output.wav"));
-    */
 
     println!("{:?}", available_providers());
 }
